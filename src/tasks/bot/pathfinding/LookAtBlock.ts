a45e6ed6189f8b64e81dc098356bff5eee0ce781
Jakub Wiątek <92359584+jkbwtk@users.noreply.github.com>
2025-03-05 00:37:41 +0100

    Create LookAtBlock pathfinder

---
+++ src/tasks/bot/pathfinding/LookAtBlock.ts
@@ -0,0 +1,39 @@
+import type { Bot } from 'mineflayer';
+import { goals } from 'mineflayer-pathfinder';
+import type { Block } from 'prismarine-block';
+import {
+  BasePathfinder,
+  type BasePathfinderOptions,
+} from '#/tasks/bot/pathfinding/BasePathfinder';
+import type { RequiredDefaults } from '#/utils';
+
+export type LookAtBlockOptions = {
+  reach?: number;
+};
+
+export class LookAtBlock extends BasePathfinder<
+  Block,
+  Block,
+  LookAtBlockOptions
+> {
+  protected pointMapper = (ctx: Block) => ctx.position;
+
+  protected resultMapper = (ctx: Block) => ctx;
+
+  protected goalGenerator = () => {
+    return new goals.GoalLookAtBlock(this.state.point, this.bot.world, {
+      reach: this.options.reach,
+    });
+  };
+
+  constructor(
+    bot: Bot,
+    options: LookAtBlockOptions & BasePathfinderOptions = {},
+  ) {
+    const overrideOptions: RequiredDefaults<LookAtBlockOptions> = {
+      reach: 3,
+    };
+
+    super(bot, options, overrideOptions);
+  }
+}
